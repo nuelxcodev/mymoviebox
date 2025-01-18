c58c574720a1b6db5f2f20326750a468f8f9c42d
ekine_chukwuemeka_emmanuel <chukwuemekacodev@gmail.com>
2025-01-18 23:29:08 +0100

    server

--- nufy_server/index.js
+++ nufy_server/index.js
@@ -5,9 +5,9 @@ const bodypasser = express.json;
 const cors = require("cors");
 const { databaseconnection } = require("./utils/helpers");
 
-require("dotenv").config({path:'../.env'});
+require("dotenv").config({ path: "../.env" });
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 8081;
 app.use(bodypasser());
 app.use(
   cors({
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 app.use("/api/v1", router);
 
-app.listen(PORT, '0.0.0.0',() => {
+app.listen(PORT, "0.0.0.0", () => {
   // databaseconnection()
   console.log(`project is now running on ${PORT}`);
 });
