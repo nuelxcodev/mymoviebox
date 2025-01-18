17832d8ba43c807b539ce0e3054896c329593e28
nuelxcodev <144784983+nuelxcodev@users.noreply.github.com>
2025-01-18 18:37:47 +0100

    Update index.js

--- nufy_server/index.js
+++ nufy_server/index.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 app.use("/api/v1", router);
 
-app.listen(PORT, () => {
+app.listen(PORT, 0.0.0.0,() => {
   // databaseconnection()
   console.log(`project is now running on ${PORT}`);
 });
