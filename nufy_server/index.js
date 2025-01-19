12f434aa2057e62008b1d12007e63e2fa49e3cf0
ekine_chukwuemeka_emmanuel <chukwuemekacodev@gmail.com>
2025-01-19 12:28:41 +0100

    new

--- nufy_server/index.js
+++ nufy_server/index.js
@@ -11,6 +11,13 @@ const PORT = process.env.PORT || 8000;
 const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 
 // Middleware
+const frontendPath = path.join(__dirname, "../dist");
+app.use(express.static(frontendPath));
+
+// Fallback Route for SPA
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 app.use(express.json()); // Parse JSON bodies
 app.use(
   cors({
@@ -34,13 +41,6 @@ app.get("/api/v1", (req, res) => {
 });
 
 // Serve Static Files for Frontend
-const frontendPath = path.join(__dirname, "../dist");
-app.use(express.static(frontendPath));
-
-// Fallback Route for SPA
-app.get("*", (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
-});
 
 // Database Connection and Server Start
 databaseconnection(); // Make sure this connects successfully
